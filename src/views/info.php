4594ff59c00e6cf6721439b40ebfefe384229477
Toby Mackenzie <public@tobymackenzie.com>
2016-09-22 02:15:09 -0400

    updated text on info pages

--- src/views/info.php
+++ src/views/info.php
@@ -1,13 +1,17 @@
 <div class="main-text"><?php
 	?><h1>Game Info</h1><?php
-	?><p>Conway's Game of Life is not a game that's playable per-se, but more of a mathematical game or simple simulation.  It consists of a grid of "cells" that can be alive or dead.  An initial pattern of cells is set up, called the seed.  Time is then incremented through a series of generations or ticks.  Specific rules define what happens to each cell based on its neighbors at each tick.  Neighbors are directly adjacent in eight directions.  The rules:</p><?php
+	?><p><a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway's Game of Life</a> is not a game that's playable per-se, but more of a mathematical game.  An example of a <a href="https://en.wikipedia.org/wiki/Cellular_automaton">cellular automaton</a>, it is sort of a simple simulation of the life of cells over time.  It consists of a grid of cells that can be alive or dead.  The cells are set up in an initial pattern, called the seed.  Time is then incremented through a series of generations or ticks.  Specific rules define what happens to each cell based on its neighbors at each tick.  Neighbors are directly adjacent in eight directions.  The rules:</p><?php
 	?><ul><?php
 		?><li>Alive cells with less than two neighbors die of under-population</li><?php
 		?><li>Alive cells with more than three neighbors die of over-population</li><?php
 		?><li>Dead cells with three neighbors become alive, replaced by reproduction</li><?php
 		?><li>Other cells remain as they were</li><?php
 	?></ul><?php
-	?><p>Various patterns can appear on the grid.  Run through many iterations and look for patterns in the cells.</p><?php
+	?><p>Various patterns can appear on the grid.  These are studied, and even given names, like glider, block, and beacon.  They can be grouped into categories, such as:</p><?php
+	?><ul><?php
+		?><li>still lifes: non-moving</li><?php
+		?><li>oscillators: move in place</li><?php
+		?><li>spaceships: travel across the grid</li><?php
+	?></ul><?php
 	?><p>The game makes for a good simple programming exercise.  As such, it is used for <a href="http://coderetreat.org/">Code Retreat</a>, an annual day-long event of programming practice.  It helps develop or hone skills, particularly focusing on software development and design fundamentals, modularity, and object-orientation.</p><?php
-	?><p><a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">More info on the game at Wikipedia</a></p><?php
 ?></div>
--- src/views/project.php
+++ src/views/project.php
@@ -1,5 +1,14 @@
 <div class="main-text"><?php
 	?><h1>Project Info</h1><?php
 	?><p>This project is built by <a href="https://www.tobymackenzie.com/">Toby Mackenzie</a>.  Toby is a quiet <abbr title="Linux Apache MySQL PHP">LAMP</abbr> full-stack web developer from Northeast Ohio.  He works for <a href="https://cogneato.com/">Cogneato</a> in Akron on many websites.</p><?php
-	?><p>This project is built for the <a href="https://a-k-apart.com/">10k Apart Challenge</a>.  It is a progressively-enhanced web implementation of <a href="<?=$this->getRelativeUrlBase()?>/info/">Conway's Game of Life</a>.  Toby encountered the game at a <a href="http://coderetreat.org/">Code Retreat event</a> and has been interesting in making a web-based version ever since.  This challenge provided the perfect opportunity.  The code for this project can be found in <a href="https://github.com/tobymackenzie/site-10k-gol">it's GitHub repo</a>.</p><?php
+	?><p>This project is built for the <a href="https://a-k-apart.com/">10k Apart Challenge</a>.  It is a progressively-enhanced web implementation of <a href="<?=$this->getRelativeUrlBase()?>/info/">Conway's Game of Life</a>.  All basic functionality works without JS or CSS</p><?php
+	?><p>Toby encountered the game at a <a href="http://coderetreat.org/">Code Retreat event</a> and has been interesting in making a web-based version ever since.  This challenge provided the perfect opportunity.  The code for this project can be found in <a href="https://github.com/tobymackenzie/site-10k-gol">it's GitHub repo</a>.</p><?php
+	?><h2>Development highlights</h2><?php
+	?><ul><?php
+		?><li>game implemented on both server and client so it can work for no-js browsers but work faster if js available.</li><?php
+		?><li>grid consists of 'X's and 'O's for text browsers.</li><?php
+		?><li>CSS inlined on first load for fast render, external sheet lazy loaded.  Cookie sent from server switches inline CSS to `&lt;link&gt;` for smaller transfer on subsequent visits.</li><?php
+		?><li>JS runs back to IE 8.</li><?php
+		?><li>`:target` shows settings panel without js.  clickable screen overlay hides it without link on panel.</li><?php
+	?></ul><?php
 ?></div>
