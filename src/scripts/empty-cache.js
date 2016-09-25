0f912c7be80cebe61404727613db8224cdfde592
Toby Mackenzie <public@tobymackenzie.com>
2016-09-25 01:13:32 -0400

    remove unnecessary wrappers and functionality for `loadJS` and `loadCSS`, since we're only loading one of each

--- src/scripts/empty-cache.js
+++ src/scripts/empty-cache.js
@@ -1,17 +1,12 @@
 (function(_d){
 	//--lazy load css into cache if we have no cache yet.  Means it should be there on next load but we don't have to wait for it on this load
 	//-@ based on https://github.com/filamentgroup/loadCSS/
-	var _cacheCSS = function(_href, _media, _neighbor){
+	setTimeout(function(){
 		var _link = _d.createElement('link');
-		if(!_neighbor){
-			_neighbor = _d.getElementsByTagName('script')[0];
-		}
+		var _neighbor = _d.getElementsByTagName('script')[0];
 		_link.rel = 'stylesheet';
-		_link.href = _href;
+		_link.href = TJM.baseUrl + '/main.css?v=' + TJM.v;
 		_link.media = 'only x';
 		_neighbor.parentNode.insertBefore(_link, _neighbor);
-	};
-	setTimeout(function(){
-		_cacheCSS(TJM.baseUrl + '/main.css?v=' + TJM.v);
-	}, 800);
+	}, 1000);
 })(window.document);
--- src/scripts/pre.js
+++ src/scripts/pre.js
@@ -4,17 +4,10 @@
 	//-# js is virtually worthless for opera mini, play button bad, so don't load it
 	if('querySelectorAll' in _d && !(_w.operamini && ({}).toString.call(_w.operamini) === '[object OperaMini]')){
 		/*! loadJS: [c]2014 @scottjehl, Filament Group, Inc. Licensed MIT */
-		var _loadJS = function(_src, _cb){
-			var _ref = _d.getElementsByTagName('script')[0];
-			var _script = _d.createElement('script');
-			_script.src = _src;
-			_script.async = true;
-			_ref.parentNode.insertBefore(_script, _ref);
-			if(_cb && typeof(_cb) === 'function') {
-				_script.onload = _cb;
-			}
-			return _script;
-		};
-		_loadJS(TJM.baseUrl + '/main.js?v=' + TJM.v);
+		var _ref = _d.getElementsByTagName('script')[0];
+		var _script = _d.createElement('script');
+		_script.src = TJM.baseUrl + '/main.js?v=' + TJM.v;
+		_script.async = true;
+		_ref.parentNode.insertBefore(_script, _ref);
 	}
 })(window, window.document);
